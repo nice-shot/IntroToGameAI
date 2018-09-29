0d6bc423d4b068c8d68043c146e586396d34f191
nice-shot <shefferu@gmail.com>
2018-09-29 11:13:33 +0300

    created a template for the html document to make the game building easier

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,41 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const games = [
+	{
+		entry: 'minigame1',
+		src: './src/games/minigame1.js',
+		name: 'Example A - Game Loop',
+		description: 'This is a game thingy...',
+	},
+	{
+		entry: 'minigame2',
+		src: './src/games/minigame2.js',
+		name: 'Example B - Straight Navigation',
+		description: 'Bla bla bli',
+	},
+]
 
 module.exports = {
-	entry: {
-		minigame1: './src/games/minigame1.js',
-		minigame2: './src/games/minigame2.js',
-		minigame3: './src/games/minigame3.js',
-		minigame4: './src/games/minigame4.js',
-	},
+	entry: games.reduce((entries, game) => {
+		entries[game.entry] = game.src
+		return entries
+	}, {}),
 	plugins: [
 		new CleanWebpackPlugin(['./dist']),
 		new CopyWebpackPlugin([
 			'public',
 		]),
+		new HtmlWebpackPlugin({
+			inject: false,
+			template: './src/templates/index.html',
+			templateParameters: {
+				games: games,
+				title: 'Intro to AI in Games - Examples',
+			},
+		}),
 	],
 	output: {
 		filename: 'js/[name].js',
